18fd4e5cc93494a02184d8c121e2992337e0822f
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-10-01 08:47:22 -0400

    Archiver
    The Archiver archives any JSON left over, making it good for packaged APIs.

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
+import json
 
+import archiver
+class Archive(self):
+  
+  def Archive(self):
+    json = """ 
+    py = ["class Archive():", "json = rawInput()", "json.Archive(self)"]
+    """
+    json.length()
+    json.archive(self)
