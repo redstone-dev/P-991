6b910eb4ea6d1bf17f72dff716765cf2bae06059
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-10-01 09:03:27 -0400

    Update and rename packages.js to packager.js

---
+++ packager.js
@@ -0,0 +1,10 @@
+function package(name){
+  function Rand(number1, number2, number3, number4){
+    var rand = Math.random(number1, number2, number3, number4) * 100;
+  }
+ var packages = "#0x" + Rand("00", "01", "02", "03")
+ function getNumberOfPackages(){
+  var i;
+  
+ }
+}
